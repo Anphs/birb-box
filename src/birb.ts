0452ceb60358565b4d327e8e7b7f3de06bba4dfa
Anphs <65219850+Anphs@users.noreply.github.com>
2025-04-15 10:50:32 -0500

    rotate birb texture

--- src/birb.ts
+++ src/birb.ts
@@ -3,9 +3,9 @@ import { Graphics, Renderer, Texture } from "pixi.js";
 export function createBirbTexture(renderer: Renderer): Texture {
   const birb = new Graphics();
 
-  birb.moveTo(0, -8);
-  birb.lineTo(8, 8);
-  birb.lineTo(0, 4);
+  birb.moveTo(8, 0);
+  birb.lineTo(-8, -8);
+  birb.lineTo(-4, 0);
   birb.lineTo(-8, 8);
   birb.closePath();
 
